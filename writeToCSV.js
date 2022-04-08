b03e828a56b5522b07b0e8e3ca4b7974d04a4246
Umang Sharma <umang.sharma@cybernetyx.com>
2022-04-08 07:20:59 +0530

    Adding output_data directory to gitignore

--- writeToCSV.js
+++ writeToCSV.js
@@ -2,7 +2,7 @@ const ObjectsToCsv = require('objects-to-csv')
 
 const writeToCsv = async (licenseDetails) => {
     const csv = new ObjectsToCsv(licenseDetails);
-    await csv.toDisk('./output-data/licenseDetails.csv');
+    await csv.toDisk('./output_data/licenseDetails.csv');
     console.log('Writing to file complete.');
 }
 
