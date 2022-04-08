8221bb2eef4ad059f8013ddcfb425608ec07c335
Umang Sharma <umang.sharma@cybernetyx.com>
2022-04-08 12:30:56 +0530

    Updating the return statement

--- decrypt.js
+++ decrypt.js
@@ -4,13 +4,13 @@ const { decryptBigBuffer } = require("node-buffer-crypto");
 const config = require('./config');
 const PRIVATE_KEY = config.PRIVATE_KEY;
 
-const decrypt = (encryptedData) => {
+const decrypt = (encryptedData) => (
     deserialize(
         decryptBigBuffer(
             PRIVATE_KEY,
             encryptedData
         )
     )
-}
+)
 
 module.exports = decrypt;
